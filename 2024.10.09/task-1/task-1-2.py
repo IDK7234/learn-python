9b2acd19c2ff8d8b04355af02543beaaf9be6ba5
IDK7234 <clewe2008@mail.ru>
2024-10-09 21:07:40 +0300

    Update task-1-2.py

--- 2024.10.09/task-1/task-1-2.py
+++ 2024.10.09/task-1/task-1-2.py
@@ -1,16 +1,21 @@
 def sep(line: str, sep):
-    a: str = ""
-    x = line.replace(" ", "")
-    for i in x:
-        a += i
-        if i in sep:
-            if a != x:
-                a += " "
+    result: list[str] = ['']
+    c = True
+    for i in line:
+        if c:
+            if i in sep:
+                c = False
 
-    z = a.split(" ")
-    z.pop(-1)
-    return z
+        else:
+            if i not in sep and i != " ":
+                result.append('')
+                c = True
+
+        result[-1] += i
+
+    return result
 
 if __name__ == '__main__':
-    a2 = [';', '.']
-    print(sep("Строка1; Строка2. Строка3.", a2))
+    separators: list[str] = [';', '.', '!', '?']
+    test_string = "Что?.. Да! Вышел корень из тумана; Вынул ножик из кармана. Раз, два, всё?.."
+    print(sep(test_string, separators))
