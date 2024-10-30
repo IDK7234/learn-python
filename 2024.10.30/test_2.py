53b4274f181bd1ffae3042cbb932f5e52d39371e
IDK7234 <clewe2008@mail.ru>
2024-10-30 20:35:02 +0300

    task 03 done
    Задача 2024.10.30.03
    
    Напишите тест, который проверяет, что в списке находятся только строки, состоящие из одного слова, без пробелов, цифр или знаков препинания.

--- 2024.10.30/test_2.py
+++ 2024.10.30/test_2.py
@@ -1,4 +1,3 @@
-
 def is_valid(word: str) -> bool:
     return word.isalpha()
 
@@ -11,3 +10,10 @@ def test():
     assert is_valid("") == False
 
 
+def test_all_words():
+    # достать все слова
+    with open("words.txt", 'r', encoding="UTF-8") as file:
+        # пройтись циклом по каждому
+        for word in file:
+            # проверить каждое с помощью assert
+            assert is_valid(word.strip()) == True
