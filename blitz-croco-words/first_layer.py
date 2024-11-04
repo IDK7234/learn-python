12adc02abbf518724903561f97201263f8210db3
IDK7234 <clewe2008@mail.ru>
2024-11-04 11:29:59 +0300

    task_2
    Задача 2024.11.01.03
    
    Продолжаем рефакторить код: вынесите проверку валидности строки, получаемой из презентации, в отдельную функцию.

--- blitz-croco-words/first_layer.py
+++ blitz-croco-words/first_layer.py
@@ -3,6 +3,14 @@ from pathlib import Path
 from zipfile import ZipFile
 from second_layer import getting_words
 
+def without_duplicates(a:list):
+    words_without_duplicates = []
+    for i in range(len(a)):
+        if a[i] not in words_without_duplicates:
+            words_without_duplicates.append(a[i])
+    return words_without_duplicates
+
+
 def parsing(name):
     words: list = []
     words_not_split = ""
@@ -14,15 +22,12 @@ def parsing(name):
     words = words_not_split.split('\n')
     words.pop()
 
-    words_without_duplicates = []
-    for i in range(len(words)):
-        if words[i] not in words_without_duplicates:
-            words_without_duplicates.append(words[i])
-
     for i in range(len(words)):
         print(f"[{i + 1}] {words[i]}")
 
     print("____________________________________________")
 
-    for i in range(len(words_without_duplicates)):
-        print(f"[{i + 1}] without_duplicates {words_without_duplicates[i]}")
+    words_n_d = without_duplicates(words)
+
+    for i in range(len(words_n_d)):
+        print(f"[{i + 1}] without_duplicates {words_n_d[i]}")
--- blitz-croco-words/second_layer.py
+++ blitz-croco-words/second_layer.py
@@ -1,6 +1,6 @@
 from pptx import Presentation
 
-def getting_words(a: []):
+def getting_words(a: []): #тенически оно и так в отдельной функции
     words = ""
     x = Presentation(a)
     for slide in x.slides:
