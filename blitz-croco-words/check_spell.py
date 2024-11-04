6e61013a8af12897faabfd869e8cc9f0e6260eab
IDK7234 <clewe2008@mail.ru>
2024-11-04 12:59:46 +0300

    Update check_spell.py

--- blitz-croco-words/check_spell.py
+++ blitz-croco-words/check_spell.py
@@ -1,12 +1,9 @@
 import pyaspeller
 
 def check(a:list):
-    text = ""
-    for i in range(len(a)):
-        text += a[i] + " "
-    text_new = pyaspeller.YandexSpeller().spelled(text)
-    text_new.split(" ")
-    return text_new
+
+    return pyaspeller.YandexSpeller().spelled(' '.join(a))
+
 
 if __name__ == '__main__':
     a: list = ["Колакал", "Малако"]
