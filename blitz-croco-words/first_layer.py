fb531d8cf26c09c2a8923d6668856ddffb20abc6
IDK7234 <clewe2008@mail.ru>
2024-11-04 11:13:57 +0300

    task_1 2024.11.04
    Задача 2024.11.01.02
    
    В какой контейнер данных нужно собирать слова чтобы исключить дубликаты?
    Поменяйте код так, чтобы это сделать.
    
    Подумайте на каком уровне лучше это делать и почему.
    
    Напишите сколько было слов всего из всех файлов, когда вы сохраняли их в список, и сколько стало, когда вы исключили дубликаты.

--- blitz-croco-words/first_layer.py
+++ blitz-croco-words/first_layer.py
@@ -14,5 +14,15 @@ def parsing(name):
     words = words_not_split.split('\n')
     words.pop()
 
+    words_without_duplicates = []
+    for i in range(len(words)):
+        if words[i] not in words_without_duplicates:
+            words_without_duplicates.append(words[i])
+
     for i in range(len(words)):
         print(f"[{i + 1}] {words[i]}")
+
+    print("____________________________________________")
+
+    for i in range(len(words_without_duplicates)):
+        print(f"[{i + 1}] without_duplicates {words_without_duplicates[i]}")
