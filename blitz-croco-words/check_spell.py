0c13b9e5eec05ce83cbdc66327b080987d0369cf
IDK7234 <clewe2008@mail.ru>
2024-11-04 13:30:33 +0300

    task_3
    Задача 2024.11.04.06
    
    Дополните код проекта blitz-croco-words функцией, которая на вход получает список слов, отправляет их в Yandex Speller, и возвращает список проверенных и отредактированных слов.
    
    Вставьте использование этой функции между парсингом презентаций и сохранением в words.txt

--- blitz-croco-words/check_spell.py
+++ blitz-croco-words/check_spell.py
@@ -1,10 +1,5 @@
 import pyaspeller
 
-def check(a:list):
+def check(a:list) -> list:
+    return pyaspeller.YandexSpeller().spelled(' '.join(a)).split(" ")
 
-    return pyaspeller.YandexSpeller().spelled(' '.join(a))
-
-
-if __name__ == '__main__':
-    a: list = ["Колакал", "Малако"]
-    print(check(a))
--- blitz-croco-words/first_layer.py
+++ blitz-croco-words/first_layer.py
@@ -2,14 +2,18 @@ import os
 from pathlib import Path
 from zipfile import ZipFile
 from second_layer import getting_words
+from check_spell import check
 
-def without_duplicates(a:list):
+def without_duplicates(a: list):
     words_without_duplicates = []
     for i in range(len(a)):
         if a[i] not in words_without_duplicates:
             words_without_duplicates.append(a[i])
     return words_without_duplicates
 
+def open_write(name:str, encouding1:str, info:list):
+    with open(name, 'w', encoding=encouding1) as file:
+        file.write("\n".join(info))
 
 def parsing(name):
     words: list = []
@@ -19,15 +23,20 @@ def parsing(name):
         for f in archive.namelist():
             words_not_split += (getting_words(archive.open(f)))
 
-    words = words_not_split.split('\n')
+    words = check(words_not_split.split('\n'))
     words.pop()
+
+    open_write("words_with_dublicates.txt", "utf-8", words)
 
     for i in range(len(words)):
         print(f"[{i + 1}] {words[i]}")
 
+
     print("____________________________________________")
 
-    words_n_d = without_duplicates(words)
+    words_n_d = check(without_duplicates(words))
+
+    open_write("words_without_dublicate.txt", "utf-8", words_n_d)
 
     for i in range(len(words_n_d)):
         print(f"[{i + 1}] without_duplicates {words_n_d[i]}")
