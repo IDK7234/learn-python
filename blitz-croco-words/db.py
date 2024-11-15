b2a4058c381cd2f95c23f02965eacb37d85ef70a
IDK7234 <clewe2008@mail.ru>
2024-11-15 21:24:43 +0300

    SQL DB add_data create

--- blitz-croco-words/db.py
+++ blitz-croco-words/db.py
@@ -2,21 +2,26 @@ import sqlite3
 from multiprocessing.connection import Connection
 
 
-def add_data(con: Connection, name:str, dic:dict[str : str]) -> None:
-    ...
+def add_data(con: sqlite3.Connection, name:str, dic: {str,str}) -> None:
+    cur = con.cursor()
+    cur.executemany(f"INSERT INTO {name} VALUES(?, ?)", dic)
+    con.commit()
+    cur.close()
+
 
 def main():
     con = sqlite3.connect('words.db')
     cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS movie(title, year, score)")
+    name = "movie_new"
+    cur.execute(f"CREATE TABLE IF NOT EXISTS {name}(title, year)")
 
-    data = [
-        ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
-        ("Monty Python's The Meaning of Life", 1983, 7.5),
-        ("Monty Python's Life of Brian", 1979, 8.0),
-    ]
-    cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
-    con.commit()
+    data = {
+        ("Monty Python Live at the Hollywood Bowl", 1982),
+        ("Monty Python's The Meaning of Life", 1983),
+        ("Monty Python's Life of Brian", 1979),
+    }
+
+    add_data(con, name, data)
 
 
 if __name__ == '__main__':
